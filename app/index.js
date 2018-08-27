06ccc6b968cbd7cea3b11f110b904ad115b24f5e
Dremkoff <n.dremkoff@gmail.com>
2018-08-27 14:08:53 +0300

    initial state

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './Root'
+
+const render = () => ReactDOM.render(<Root/>, document.getElementById('root'))
+
+render()
+
+if(module.hot){
+	module.hot.accept('./Root', () => {
+		render()
+	});
+}
